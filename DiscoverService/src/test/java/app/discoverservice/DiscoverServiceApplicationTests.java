f8d58910f5ed038e5759e0695d8b00b1b478cb50
Andres <142335765+andreUmag@users.noreply.github.com>
2024-10-30 13:16:00 -0500

    Modulo de Discovery Service

---
+++ DiscoverService/src/test/java/app/discoverservice/DiscoverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.discoverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
